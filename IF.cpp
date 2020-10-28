809f5b63577410e0e907f40c8499b79c7a196677
Thiru-01 <51126340+Thiru-01@users.noreply.github.com>
2020-10-28 10:05:29 +0530

    Update IF.cpp

--- IF.cpp
+++ IF.cpp
@@ -7,7 +7,7 @@ inline int cube(int s)
         return s*s*s; 
     }
 
-inline int inc(int a) 
+inlint inc(int a) 
     { 
         return a*a; 
     }
@@ -16,7 +16,7 @@ int main()
 
     { 
         int a = 2;
-        cout << "\nThe cube of 3 is: " << cube(3); 
+        cout << "\nThe cube of 3 is: " << cube(3); //s*s*s*;
         cout << "\nThe square of 2 is: " << inc(a);
         getch();
         return 0; 
